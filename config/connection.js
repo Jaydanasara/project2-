d5513026a98ceb574ea5ab33e3d7ce2c53efc33b
jay <jaydanasara@yahoo.com>
2018-12-23 22:47:24 -0500

    herokufinal

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,8 @@
 var mysql = require("mysql");
 var connection;
-if (process.env.JAWsDB_URL) {
-  connection = mysql.createConnection
-}
-else{
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
 connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
